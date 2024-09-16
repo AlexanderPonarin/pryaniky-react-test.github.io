0aeb952e7fed6deee2195f7eb9eff1eb2b8cca5e
AlexanderPonarin <114759942+AlexanderPonarin@users.noreply.github.com>
2024-09-16 11:57:52 +0300

    Update success-handler-middleware.ts

--- src/store/redux/middleware/success-handler-middleware.ts
+++ src/store/redux/middleware/success-handler-middleware.ts
@@ -1,35 +1,35 @@
-import { Action, Dispatch, Store } from '@reduxjs/toolkit';
+import { Middleware } from '@reduxjs/toolkit';
 import { setSuccessMsg } from '../slices/auth';
 
-type Middleware = (store: Store) => (next: Dispatch) => (action: Action) => void;
 
-interface ActionWithPayload extends Action {
+interface  ActionProps {
+  type: string
   payload: {
     data: {
       data: {
         token: string;
       };
     };
-  };
+  }
 }
 
-export const successHandlerMiddleware: Middleware = (store) => (next) => (action: Action) => {
-  if (action) {
-    if (action.type === 'documents/fetchRemoveDocument/fulfilled') {
+export const successHandlerMiddleware: Middleware = (store) => (next) => (action: unknown) => {
+  if ( action as ActionProps) {
+    if ((action as ActionProps)?.type === 'documents/fetchRemoveDocument/fulfilled') {
       store.dispatch(setSuccessMsg('Документ успешно удален'));
     }
-    if (action.type === 'documents/fetchPatchDocument/fulfilled') {
+    if ((action as ActionProps)?.type === 'documents/fetchPatchDocument/fulfilled') {
       store.dispatch(setSuccessMsg('Документ успешно отредактирован'));
     }
-    if (action.type === 'documents/fetchCreateDocument/fulfilled') {
+    if ((action as ActionProps)?.type === 'documents/fetchCreateDocument/fulfilled') {
       store.dispatch(setSuccessMsg('Документ успешно создан'));
     }
-    if (action.type === 'auth/fetchAuth/fulfilled') {
-      if ((action as ActionWithPayload).payload.data?.data?.token) {
+    if ((action as ActionProps).type === 'auth/fetchAuth/fulfilled') {
+      if ((action as ActionProps).payload?.data?.data?.token) {
         store.dispatch(setSuccessMsg('Авторизация прошла успешно'));
       }
     }
-    return next(action);
   }
+
   return next(action);
 };
